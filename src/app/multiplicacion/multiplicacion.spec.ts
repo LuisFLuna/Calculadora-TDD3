f11e6081f83a00afba7a000ffe5da2bf7c603107
LuisFLuna <luigifleed2@hotmail.com>
2019-02-07 13:22:44 -0600

    Changed name of function in test

--- src/app/multiplicacion/multiplicacion.spec.ts
+++ src/app/multiplicacion/multiplicacion.spec.ts
@@ -2,21 +2,21 @@ import { multiplicacion } from './multiplicacion';
 
 describe('multiplicacion', () => {
   it('it should have 2 numbers', () => {
-    expect(resta(1,1)).toBe(1);
+    expect(multiplicacion(1,1)).toBe(1);
   })
   it('it should give negative numbers', () => {
-    expect(resta(2,-1)).toBe(-2);
+    expect(multiplicacion(2,-1)).toBe(-2);
   })
   it('it should multiply a number by 0 correctly', () => {
-    expect(resta(2,0)).toBe(0);
+    expect(multiplicacion(2,0)).toBe(0);
   })
   it('it should change the sign if they are negative', () => {
-    expect(resta(-2,-2)).toBe(4);
+    expect(multiplicacion(-2,-2)).toBe(4);
   })
   it('it should multiply by decimal numbers correctly', () => {
-    expect(resta(2,0.5)).toBe(1);
+    expect(multiplicacion(2,0.5)).toBe(1);
   })
   it('it should be constant with 0', () => {
-    expect(resta(0,0)).toBe(0);
+    expect(multiplicacion(0,0)).toBe(0);
   })
 });
